d9d2fce2c96679e58b4fe30bd91a0142f90deb4e
= <kennylalek@gmail.com>
2024-10-27 18:21:59 +0100

    fix

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index() :
-    return redirect("<h1>Hello World</h1>")
+    return ("<h1>Hello World</h1>")
 
 @app.route('/networks/<lab>') 
 def lab(lab) :
